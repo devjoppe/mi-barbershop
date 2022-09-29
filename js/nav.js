c90b72fc897e1be2b629319d5b12b0f8966bb898
devjoppe <joakim.ottosson@medieinstitutet.se>
2022-09-29 16:41:25 +0200

    initial commit

---
+++ js/nav.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu");
+const btnMenu= document.querySelector(".btn-menu");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+btnMenu.addEventListener("click", toggleMenu);
